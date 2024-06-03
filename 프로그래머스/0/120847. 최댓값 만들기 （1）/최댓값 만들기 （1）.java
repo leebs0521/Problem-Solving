c4757ec9dfac0bf4d9a4feae10c2591fcebb9d4e
Lee Beom-Su <leebs0521@gmail.com>
2024-06-03 11:13:21 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.42 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int length = numbers.length;
+        return numbers[length-1] * numbers[length-2];
+    }
+}
