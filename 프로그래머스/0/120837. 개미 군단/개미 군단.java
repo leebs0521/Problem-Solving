2638c390bdb92c5bf0873c5a5a072007a0215450
Lee Beom-Su <leebs0521@gmail.com>
2024-06-04 17:15:24 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+
+        if (hp >= 5) {
+            answer += hp/5;
+            hp %= 5;
+        }
+
+        if (hp >= 3) {
+            answer += hp/3;
+            hp %= 3;
+        }
+        
+        if (hp >= 1) {
+            answer += hp;
+            hp %= 1;
+        }
+        return answer;
+    }
+}
