d1d8ffacc224c35792811c727872d3ff4986d24c
Beom-Su Lee <leebs0521@gmail.com>
2024-06-22 14:25:40 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,12 +1,12 @@
 class Solution {
     public long solution(long n) {
         long answer = (long) Math.sqrt(n);
-        if (answer*answer == n) {
-            answer = (answer+1) * (answer+1);
+        if (Math.pow(answer, 2) == n) {
+            answer = (long) Math.pow(answer+1, 2);
         } else {
             answer = -1;
         }
-        
+
         return answer;
     }
 }
