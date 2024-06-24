be5fe95fa17c80f7f8d57559a5edf2c09998dfa3
Beom-Su Lee <leebs0521@gmail.com>
2024-06-24 19:36:19 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -5,56 +5,45 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        List<Edge>[] graph = new ArrayList[n + 1];
-        for (int i=1; i<=n; i++) {
-            graph[i] = new ArrayList<>();
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
         }
 
-        for (int i=0; i<m; i++) {
+        for (int i = 0; i < m; i++) {
             String[] inputs = br.readLine().split(" ");
             int start = Integer.parseInt(inputs[0]);
             int end = Integer.parseInt(inputs[1]);
-            graph[start].add(new Edge(end));
-            graph[end].add(new Edge(start));
+            graph.get(start).add(end);
+            graph.get(end).add(start);
         }
 
-        boolean[] isVisited = new boolean[n + 1];
-        isVisited[1] = true;
-        Deque<Integer> dq = new LinkedList<>();
-        dq.add(1);
+        boolean[] visited = new boolean[n + 1];
+        visited[1] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
 
-        while (!dq.isEmpty()) {
-
-            int cur = dq.pollFirst();
-
-            for (Edge next : graph[cur]) {
-                if (!isVisited[next.to]) {
-                    dq.add(next.to);
-                    isVisited[next.to] = true;
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int next : graph.get(cur)) {
+                if (!visited[next]) {
+                    queue.offer(next);
+                    visited[next] = true;
                 }
             }
         }
 
-        int cnt = 0;
-        for (int i=2; i<=n; i++) {
-            if(isVisited[i]) {
-                cnt++;
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (visited[i]) {
+                count++;
             }
         }
 
-        bw.write(cnt+"");
-        bw.flush();
-    }
-}
-
-class Edge {
-    int to;
-
-    public Edge(int to) {
-        this.to = to;
+        System.out.println(count);
     }
 }
