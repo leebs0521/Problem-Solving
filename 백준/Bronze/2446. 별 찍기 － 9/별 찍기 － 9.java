c5b8c4ab454ec321a8d968b9421cb1a68a90581e
Beom-Su Lee <leebs0521@gmail.com>
2024-07-11 20:57:17 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 108 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main  {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            bw.write(" ".repeat(i));
+            bw.write("*".repeat(2*N - 1 - 2*i) + "\n");
+        }
+
+        for (int i = N-2; i >= 0; i--) {
+            bw.write(" ".repeat(i));
+            bw.write("*".repeat(2*N - 1 - 2*i) + "\n");
+        }
+
+        bw.flush();
+    }
+}
