014bc6eb64e03ff751a05e8ca1566372d06ae3d7
Beom-Su Lee <leebs0521@gmail.com>
2024-08-02 17:18:11 +0900

    [Silver II] Title: 유기농 배추, Time: 180 ms, Memory: 16488 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int M, N, K;
+    static boolean[][] visited;
+    static int[][] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            graph = new int[M][N];
+            visited = new boolean[M][N];
+            int cnt = 0;
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st.nextToken());
+                int end = Integer.parseInt(st.nextToken());
+                graph[start][end] = 1;
+            }
+
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(visited[i][j] || graph[i][j] == 0) continue;
+                    cnt++;
+                    visited[i][j] = true;
+                    bfs(i, j);
+                }
+            }
+            bw.write(cnt+"\n");
+        }
+        bw.flush();
+    }
+    static void bfs(int i, int j){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i, j});
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for (int k = 0; k < 4; k++) {
+                int nx = cur[0] + dx[k];
+                int ny = cur[1] + dy[k];
+
+                if(nx < 0 || nx > M -1 || ny < 0 || ny > N - 1) continue;
+                if(graph[nx][ny] == 0 || visited[nx][ny]) continue;
+                visited[nx][ny] = true;
+                q.add(new int[]{nx, ny});
+            }
+        }
+    }
+}
