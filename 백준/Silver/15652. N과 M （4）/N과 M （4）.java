ebc8fa3dd90cc9d50d0fda46b346a0d7f7b33e11
Beom-Su Lee <leebs0521@gmail.com>
2024-08-05 12:25:49 +0900

    [Silver III] Title: N과 M (4), Time: 168 ms, Memory: 20336 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] ans;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        ans = new int[M];
+        perm(0, 1);
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    static void perm(int depth, int cur) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(ans[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = cur; i < N + 1; i++) {
+            ans[depth] = i;
+            perm(depth+1, i);
+        }
+    }
+}
