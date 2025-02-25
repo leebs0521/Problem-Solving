d1b729224dc7c08176781c379a094dbde631c67d
Beom-Su Lee <leebs0521@gmail.com>
2025-02-25 14:27:02 +0900

    Time: 0 ms (100%), Space: 46.7 MB (16.54%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,20 +1,22 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k = 1;
-        int cnt = 1;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i-1]) {
-                nums[k++] = nums[i];
-                cnt = 1;
-            } else {
-                cnt++;
-                if (cnt <= 2) {
-                    nums[k++] = nums[i];
-                }
-            }
+        if (nums.length <= 1) {
+            return nums.length;
         }
 
+        int k = 1; // k는 유효한 요소의 인덱스를 추적
+        for (int i = 1; i < nums.length; i++) {
+            // 현재 숫자가 이전 숫자와 다르면 배열에 새로운 숫자 추가
+            if (nums[i] != nums[i - 1]) {
+                nums[k++] = nums[i];
+            } else if (k > 1 && nums[i] == nums[k - 2]) {
+                // 현재 숫자가 이전 두 번째 숫자와 같으면, 이미 두 번 등장한 경우
+                continue;
+            } else {
+                // 두 번째 중복까지만 허용되므로 k 위치에 저장
+                nums[k++] = nums[i];
+            }
+        }
         return k;
     }
 }
