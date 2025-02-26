c6fcd4cd89c8c71a9b77e2f6613b3bee06325da0
Beom-Su Lee <leebs0521@gmail.com>
2025-02-26 14:37:07 +0900

    Time: 2 ms (70.83%), Space: 41.7 MB (51.09%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,16 +1,18 @@
-import java.util.Arrays;
-
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations); 
-        int n = citations.length;
-
-        for (int i = 0; i < n; i++) {
-            if (citations[i] >= n - i) {
-                return n - i;
+        Arrays.sort(citations);
+        int low = 0, high = citations.length;
+        
+        while (low < high) {
+            int mid = low + (high - low + 1) / 2;
+            
+            if (citations[citations.length - mid] >= mid) {
+                low = mid;  // 가능한 h-index가 mid 이상일 경우, 범위를 증가시킴
+            } else {
+                high = mid - 1;  // 그렇지 않으면 범위를 줄임
             }
         }
-
-        return 0; 
+        
+        return low;
     }
 }
