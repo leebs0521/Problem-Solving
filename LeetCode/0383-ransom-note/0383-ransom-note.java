b0739641518a67b70d62bf5af3c3148716638569
Beom-Su Lee <leebs0521@gmail.com>
2025-02-27 16:51:24 +0900

    Time: 15 ms (25.95%), Space: 45.3 MB (28.92%) - LeetHub

---
+++ LeetCode/0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : magazine.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : ransomNote.toCharArray()) {
+            int cnt = map.getOrDefault(ch, 0);
+            if (cnt == 0)
+                return false;
+
+            map.put(ch, cnt - 1);
+        }
+
+        return true;
+    }
+
+}
