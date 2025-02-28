2826c0133eb14a070b6381819e63a1e7c1c42449
Beom-Su Lee <leebs0521@gmail.com>
2025-02-28 16:35:36 +0900

    Time: 5 ms (74.21%), Space: 57.9 MB (64.31%) - LeetHub

--- LeetCode/0011-container-with-most-water/0011-container-with-most-water.java
+++ LeetCode/0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,13 +1,12 @@
 class Solution {
     public int maxArea(int[] height) {
+        int ans = 0;
         int left = 0;
         int right = height.length - 1;
 
-        int ans = 0;
-
         while (left < right) {
             int cur = (right - left) * Math.min(height[left], height[right]);
-            ans = Math.max(cur, ans);
+            ans = Math.max(ans, cur);
 
             if (height[left] > height[right]) {
                 right--;
@@ -17,6 +16,5 @@ class Solution {
         }
 
         return ans;
-
     }
 }
