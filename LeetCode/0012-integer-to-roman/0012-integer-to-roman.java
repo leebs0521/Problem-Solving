91c3deefd1b4e3d903cc8a6812724d0d686c1cd6
Beom-Su Lee <leebs0521@gmail.com>
2025-02-28 11:30:01 +0900

    Time: 3 ms (98.29%), Space: 44.6 MB (40.94%) - LeetHub

--- LeetCode/0012-integer-to-roman/0012-integer-to-roman.java
+++ LeetCode/0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,12 +1,13 @@
 class Solution {
     public String intToRoman(int num) {
-        int[] iArr = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-        String[] sArr = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int iArr[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String sArr[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
         StringBuilder ans = new StringBuilder();
+
         int idx = 0;
-        while(num > 0 && idx < iArr.length) {
-            if(num >= iArr[idx]) {
+        while (num > 0) {
+            if (num >= iArr[idx]) {
                 ans.append(sArr[idx]);
                 num -= iArr[idx];
             } else {
