e74fb4c1a19ec78733da7a801053464757b6a734
Beom-Su Lee <leebs0521@gmail.com>
2025-02-28 16:02:39 +0900

    Time: 17 ms (15.43%), Space: 44.6 MB (48.52%) - LeetHub

--- LeetCode/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        int i = 0;
-        int j = s.length() - 1;
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        while(i <= j) {
-            if(s.charAt(i++) != s.charAt(j--)) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
                 return false;
             }
         }
