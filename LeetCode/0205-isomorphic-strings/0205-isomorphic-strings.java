c810db53311cb9bafc24e4f151fe38c4be1cab5f
Beom-Su Lee <leebs0521@gmail.com>
2025-03-06 15:49:55 +0900

    Time: 14 ms (50.16%), Space: 42.4 MB (69.66%) - LeetHub

--- LeetCode/0205-isomorphic-strings/0205-isomorphic-strings.java
+++ LeetCode/0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> charMap = new HashMap<>();
+        Map<Character, Character> map = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
             char sch = s.charAt(i);
             char tch = t.charAt(i);
 
-            if (charMap.containsKey(sch)) {
-                if (charMap.get(sch) != tch) {
+            if (map.containsKey(sch)) {
+                if (map.get(sch) != tch) {
                     return false;
                 }
-            } else if (charMap.containsValue(tch)) {
+            } else if (map.containsValue(tch)) {
                 return false;
             }
 
-            charMap.put(sch, tch);
+            map.put(sch, tch);
         }
 
-        return true;        
+        return true;
     }
 }
