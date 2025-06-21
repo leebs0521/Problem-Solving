52eba2e261d1094a1a516e79e9734493c91f54e8
Beom-Su Lee <leebs0521@gmail.com>
2025-06-21 23:04:21 +0900

    [level 2] Title: 괄호 회전하기, Time: 49.98 ms, Memory: 92 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        int n = s.length();
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            ArrayDeque<Character> stack = new ArrayDeque<>();
+            boolean isValid = true;
+
+            for (int j = 0; j < n; j++) {
+                char ch = s.charAt((i + j) % n);
+
+                if (!map.containsKey(ch)) {
+                    stack.push(ch);
+                } else {
+                    if (stack.isEmpty() || !stack.pop().equals(map.get(ch))) {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isValid && stack.isEmpty()) answer++;
+        }
+
+        return answer;
+    }
+}
